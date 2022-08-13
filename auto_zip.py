e381390a35488d298709cb6064d1e767271e23f6
gogofishman <1125404308@qq.com>
2022-08-13 16:12:30 +0800

    增加了敏感词替换功能

--- auto_zip.py
+++ auto_zip.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 import subprocess
 import traceback
@@ -7,9 +6,10 @@ import sys
 import time
 import configparser
 
-version = '1.0.1'
+version = '1.1.1'
 file_pw = ''  # 密码
 size = 0
+words = []
 config = configparser.ConfigParser()
 
 
@@ -17,17 +17,23 @@ def main(in_dir):
     print('version: ' + version)
     global file_pw
     global size
+    global words
     file_path = os.path.dirname(in_dir) + "/"  # 文件路径
     file_name = os.path.basename(in_dir)  # 文件名带后缀
     _name, houzhui = os.path.splitext(file_name)
-
+    # 敏感词替换
+    if len(words) > 0:
+        for w in words:
+            if _name.find(w)>=0:
+                print('发现敏感词:',w,",已替换")
+                _name = _name.replace(w, 'O'*len(w))
     command = 'HaoZipC a -tzip -p"{1}" "{2}{3}(内层).zip" "{0}" -mm=Copy'.format(
         in_dir, file_pw, file_path, _name)
     command2 = 'HaoZipC a -tzip -v{4}m "{2}{3}.zip" "{2}{3}(内层).zip" -mm=Copy'.format(
         in_dir, file_pw, file_path, _name, size)
     # 同路径下需要有压缩程序 HaoZipC
     print('开始压缩。。。')
-    print('设置密码————————',file_pw)
+    print('设置密码————————', file_pw)
     subprocess.run(command, shell=True, cwd=os.path.dirname(sys.executable) + "./HaoZip")
     subprocess.run(command2, shell=True, cwd=os.path.dirname(sys.executable) + "./HaoZip")
     os.remove('{0}{1}(内层).zip'.format(file_path, _name))
@@ -41,6 +47,8 @@ if __name__ == '__main__':
             config.read(exe_path + '/' + '设置.ini', encoding='utf-8')
             file_pw = config['自定义']['压缩密码']
             size = config['自定义']['分卷压缩大小']
+            words = config['自定义']['敏感词'].split('，')
+            print(words)
             main(params)
         except Exception as e:
             traceback.print_exc()
