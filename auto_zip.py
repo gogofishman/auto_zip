ca40ac19478b88209353d1635cf5555f740b169a
gogofishman <1125404308@qq.com>
2022-08-13 14:12:34 +0800

    修复ini文件读取问题

--- auto_zip.py
+++ auto_zip.py
@@ -26,6 +26,7 @@ def main(in_dir):
         in_dir, file_pw, file_path, _name, size)
     # 同路径下需要有压缩程序 HaoZipC
     print('开始压缩。。。')
+    print('设置密码————————',file_pw)
     subprocess.run(command, shell=True, cwd=os.path.dirname(sys.executable) + "./HaoZip")
     subprocess.run(command2, shell=True, cwd=os.path.dirname(sys.executable) + "./HaoZip")
     os.remove('{0}{1}(内层).zip'.format(file_path, _name))
@@ -35,7 +36,8 @@ if __name__ == '__main__':
     params = sys.argv[1]
     if os.path.exists(params):  # 检查路径是否存在
         try:
-            config.read(os.path.dirname(sys.executable) + '/' + '设置.ini', encoding='utf-8')
+            exe_path, exe_name = os.path.split(os.path.realpath(sys.argv[0]))
+            config.read(exe_path + '/' + '设置.ini', encoding='utf-8')
             file_pw = config['自定义']['压缩密码']
             size = config['自定义']['分卷压缩大小']
             main(params)
